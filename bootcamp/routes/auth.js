dba2c11b8011bea5635318e8b9bcf2cead30f623
Kira-Floris <nzafloris@gmail.com>
2022-06-07 11:48:59 +0200

    controllers routes update user and password

--- bootcamp/routes/auth.js
+++ bootcamp/routes/auth.js
@@ -1,23 +1,22 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controlers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controlers/auth');
 
 const {protect} = require('../middlewares/auth');
 
 const router = express.Router();
 
-router
-    .post('/register', register);
+router.post('/register', register);
 
-router
-    .post('/login', login);
+router.post('/login', login);
 
-router
-    .get('/me', protect, getMe);
+router.get('/me', protect, getMe);
 
-router
-    .post('/forgotpassword', forgotPassword);
+router.post('/forgotpassword', forgotPassword);
 
-router
-    .put('/resetpassword/:resettoken', resetPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
+
+router.put('/updatedetails', protect, updateDetails);
+
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
