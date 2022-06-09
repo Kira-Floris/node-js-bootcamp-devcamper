3fdab4f19b2374c1100a0f454b7a112ab225099b
Kira-Floris <nzafloris@gmail.com>
2022-06-09 11:16:49 +0200

    server.js sanitize data, security headers, xss scripting attacks, rate limiting, http param pollution, cors enabling

--- bootcamp/controlers/auth.js
+++ bootcamp/controlers/auth.js
@@ -38,14 +38,14 @@ exports.login = asyncHandler(async (req, res, next)=>{
     const user = await User.findOne({ email }).select('+password');
 
     if(!user){
-        return next(new ErrorResponse('Invalid credentials'), 401);
+        return next(new ErrorResponse('Invalid credentials', 401));
     }
 
     // check if password matches
     const isMatch = await user.matchPassword(password);
 
     if(!isMatch){
-        return next(new ErrorResponse('Invalid credentials'), 401);
+        return next(new ErrorResponse('Invalid credentials', 401));
     }
 
     // create token
--- bootcamp/server.js
+++ bootcamp/server.js
@@ -6,6 +6,11 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 // connection to mongoDB
 const connectDB = require('./config/db');
@@ -47,6 +52,25 @@ app.use(fileupload());
 
 // sanitize data
 app.use(mongoSanitize());
+
+// set security headers
+app.use(helmet());
+
+// prevent XSS scripting attacks
+app.use(xss());
+
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100
+});
+app.use(limiter);
+
+// prevent http param polution
+app.use(hpp());
+
+// enable cors makes it public 
+app.use(cors());
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
